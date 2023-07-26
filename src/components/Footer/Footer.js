19c5cfeb7f08fd488e6e1b8a146e6abe26cf9757
dk347 <134257449+dk347@users.noreply.github.com>
2023-07-26 22:37:16 +0530

    my commit

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "../../App.css";
+
+function Footer() {
+  return (
+    <footer>
+      <div className="wrapper"></div>
+      <div className="footer">
+        <div className="footer-content">
+          <div className="footer-section about">
+            <p className="footer-para">
+              Developer: <span className="my-name"> dhaval kargar</span>
+            </p>
+
+            <div className="contact"></div>
+            <div className="socials">
+
+            </div>
+            <p className="copyright">
+              Copyright&nbsp;&copy;&nbsp;Nike Shoes Template
+            </p>
+            <br />
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
